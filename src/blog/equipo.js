4c42285685424a14843c2e1f05ccbe7083f3c87b
Daniel <dani@grdar.com>
2018-09-17 20:16:17 +0200

    Page author & page our team

---
+++ src/blog/equipo.js
@@ -0,0 +1,12 @@
+// js
+import Popper from 'popper.js'
+import './assets/js/custom'
+
+import('./assets/js/news-carousel-cards')
+import('./assets/js/news-carousel')
+
+// Import bootstrap
+import './../_common/bootstrap/bootstrap.scss'
+import '../_common/bootstrap/bootstrap'
+
+import './assets/styles/custom.scss'
